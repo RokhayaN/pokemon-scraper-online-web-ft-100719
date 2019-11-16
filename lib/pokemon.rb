4fdc230e1166a98404bb9275c7b0fdc120ea91cc
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-16 01:14:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,7 @@ class Pokemon
       db.execute("INSERT INTO pokemon(id, name, type) VALUES (?,?,?)",@id, name, type)
   end 
   def self.find(id, db)
-@@ -26,8 +27,13 @@ def self.find(id, db)
+ 
       pokemon.id = pokemon_info[0]	      pokemon.id = pokemon_info[0]
       pokemon.name = pokemon_info[1]	      pokemon.name = pokemon_info[1]
       pokemon.type = pokemon_info[2]	      pokemon.type = pokemon_info[2]
